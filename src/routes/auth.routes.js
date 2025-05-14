795159e7711ce3724bf018a181616e6098a3f8f3
NguyenNhatMinh <dienhoanguyen11@gmail.com>
2025-05-15 00:58:13 +0700

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -111,14 +111,14 @@ router.post('/login', async (req, res) => {
     );
     
     res.json({
+        token,
       user: {
         id: user._id,
         username: user.username,
         fullName: user.fullName,
         email: user.email,
         role: user.role
-      },
-      token
+      }
     });
   } catch (error) {
     console.error('Login error:', error);
